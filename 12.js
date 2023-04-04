f6095c5f0ce0647d43bdea8093a9c808bce98f22
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 14:45:02 -0600

    Update 12.js

--- 12.js
+++ 12.js
@@ -5,12 +5,13 @@ let impares=0;
 
 for(let i=1; i<=100; i++){
   console.log(i);
-  if(i%2 == 0){
-    pares=pares+i;
-  }else{
-    impares=impares+i;
-  }
+    if(i%2 == 0){
+      pares=pares+i;
+    }else{
+      impares=impares+i;
+    }
+}
   
-  console.log("Suma de los números pares: " + pares);
-  console.log("Suma de los números impares: " + impares);
+console.log("Suma de los números pares: " + pares);
+console.log("Suma de los números impares: " + impares);
   
