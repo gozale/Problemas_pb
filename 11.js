5add0565b62ed5a291f2049d362d7642302acf57
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 14:29:10 -0600

    Update 11.js

--- 11.js
+++ 11.js
@@ -11,7 +11,7 @@ let cont=0;
 
 rl.question('Ingrese el número: ',(num) =>{
   is(isNaN(num)){
-    console.log("No es un número");
+    console.log("No es un número")
   }else{
     for(let i=3; i<=num; i=i+3){
       cont++;
