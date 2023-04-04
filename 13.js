4339cf298cbdd45a9931f7908258e411dc96bc51
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 16:10:20 -0600

    Update 13.js

--- 13.js
+++ 13.js
@@ -3,13 +3,13 @@ console.log("Imprimir y contar los números que son múltiplos de 2 o de 3 que h
 let cont=0;
 
 for(let i=1; i<=100; i++){
-  if(i%2==0 and i%3!=0){
+  if(i%2==0 && i%3!=0){
     console.log("Múltiplo de 2: "+i);
     cont++;
-  }elif(i%3==0 and i%2!=0){
+  }elif(i%3==0 && i%2!=0){
     console.log("Múltiplo de 3: "+i);
     cont++;
-  }elif(i%2==0 and i%3==0){
+  }elif(i%2==0 && i%3==0){
     console.log("Múlitplo de 2 y 3: "+i);
     cont++;
   }
