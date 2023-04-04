8ba29490ae9ff068a26a6280f827d0585bedaae6
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 16:22:06 -0600

    Create 14.js
    Imprime el mayor y el menor de una serie de 5 números que introduzcamos

---
+++ 14.js
@@ -0,0 +1,43 @@
+console.log("Imprime el mayor y el menor de una serie de 5 números que introduzcamos");
+
+function getMax(num_list){
+  let max= num_list[0];
+  for(let i=1; i<num_list.length; i++){
+    if(max < num_list[i]){
+      max=num_list[i];
+    }
+  }
+  return max;   
+}
+  
+function getMin(num_list){
+  let min= num_list[0];
+  for(let i=1; i<num_list.length; i++){
+    if(min > num_list[i]){
+      min=num_list[i];
+    }
+  }
+  return min;  
+}
+  
+const readline= require('readline');
+ 
+const rl= readline.createInterface({
+  input: process.stdin,
+  output: numbers.stdout
+});
+  
+let lineCount=0;
+let num_list = new Array(5);
+rl.prompt();
+
+rl.on('linea',(linea)=>{
+    num_list[lineCount] = Number.parseFloat(linea);
+    lineCount++;
+    if (lineCount == 5) {
+        rl.close();
+        console.log(num_list);
+        console.log('Número mínimo: ' + getMin(num_list));
+        console.log('Número máximo: ' + getMax(num_list));
+    }  
+})
