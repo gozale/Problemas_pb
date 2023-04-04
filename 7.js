1b11709d295b949c645f0cc159d58cd7650220d5
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 13:26:56 -0600

    Create 7.js
    Introduce tantas frases como quieras y cuentalas

---
+++ 7.js
@@ -0,0 +1,18 @@
+console.log("Introduce tantas frases como quieras y cuentalas");
+
+const readline = require('readline');
+
+const fs = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let contador=0;
+fs.on('line',()=>{
+  count++;
+}).on('close', () =>{
+  console.log('Total number of phrases: ' + contador);
+  process.exit(0);
+});
+
+fs.prompt();
