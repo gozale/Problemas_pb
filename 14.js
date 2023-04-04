497b15385b3d003201908193dd45bc8cef723306
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 16:23:50 -0600

    Update 14.js

--- 14.js
+++ 14.js
@@ -24,7 +24,7 @@ const readline= require('readline');
  
 const rl= readline.createInterface({
   input: process.stdin,
-  output: num_list.stdout
+  output: process.stdout
 });
   
 let lineCount=0;
