5b6f72431585006b3447f7362fb65b9635bb6ad4
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 13:05:17 -0600

    Update 5.js

--- 5.js
+++ 5.js
@@ -3,7 +3,7 @@ console.log("Imprime los números impares hasta el 100 e imprime cuantos impares
 let contador=0;
 
 for (let i=0; i< 50; i++){
-  console.log((2*1)+1);
+  console.log((2*i)+1);
   contador++;
 }
 console.log("Número total de impares: "+contador);
