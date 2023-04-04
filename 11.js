ff964e3e2b8d2b797c17348eb30999bac6f0eb12
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 14:27:40 -0600

    Create 11.js
    Imprime y cuenta los múltiplos de 3 desde el 1 hasta el número que se ingrese

---
+++ 11.js
@@ -0,0 +1,23 @@
+console.log("Imprime y cuenta los múltiplos de 3 desde el 1 hasta el número que se ingrese");
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let cont=0;
+
+rl.question('Ingrese el número: ',(num) =>{
+  is(isNaN(num)){
+    console.log("No es un número");
+  }else{
+    for(let i=3; i<=num; i=i+3){
+      cont++;
+      console.log(i);
+    }
+  }
+  console.log("Hubo "+cont+" múltiplos de 3");
+  rl.close();
+});
