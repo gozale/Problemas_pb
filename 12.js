7f1c4524d0471f3e4913b168a755f72fd92b63c6
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 14:41:49 -0600

    Create 12.js
    Imprime todos los números del 1 al 100 y calcula la suma de los pares y los impares

---
+++ 12.js
@@ -0,0 +1,16 @@
+console.log("Imprime todos los números del 1 al 100 y calcula la suma de los pares y los impares");
+
+let pares=0;
+let impares=0;
+
+for(let i=1; i<=100; i++){
+  console.log(i);
+  if(i%2 == 0){
+    pares=pares+i;
+  }else{
+    impares=impares+i;
+  }
+  
+  console.log("Suma de los números pares: " + pares);
+  console.log("Suma de los números impares: " + impares);
+  
