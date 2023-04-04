f698a6f5bcf42abbfdd450a35c6051d46db3660c
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 13:15:17 -0600

    Create 6.js
    Imprime todos los números naturales que hay desde el 1 hasta un número que se introduzca por teclado

---
+++ 6.js
@@ -0,0 +1,14 @@
+console.log("Imprime todos los números naturales que hay desde el 1 hasta un número que se introduzca por teclado");
+
+const readline = require('readline');
+const n= readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+n.question('Introduzca un número: ',(num) => {
+  for (let i=0; i <= num; i++){
+    console.log(i);
+  }
+  n.close();
+});
