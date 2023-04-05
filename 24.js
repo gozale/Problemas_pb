b4f80a46edc3ade0ff04a1f43bb3959d2c303a6c
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 20:01:37 -0600

    Create 24.js
    Comprobar si un número mayor o igual que la unidad es primo

---
+++ 24.js
@@ -0,0 +1,24 @@
+console.log("Comprobar si un número mayor o igual que la unidad es primo");
+
+function esPrimo(n){
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    let i = 5;
+    while(i*i <= n){
+        if (n % i == 0 || n % (i + 2) == 0 ){
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line',(linea)=>{
+    console.log("La primaridad del número es: " + isPrime(Number.parseInt(linea)));
+    rl.close();
+});
