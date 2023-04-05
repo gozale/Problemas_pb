a27f8a94f2be130fa319c52666007f735b4d29bb
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 22:17:35 -0600

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,9 @@
+module.exports = class Data{
+    constructor(id,nombre,apellido,direccion,estado){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.estado = estado;
+    }
+}
