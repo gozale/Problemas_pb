9ea6e9221a014ec7e03039d26f0d43f2e910a689
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 21:08:48 -0600

    Create 32.js
    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta

---
+++ 32.js
@@ -0,0 +1,27 @@
+console.log("Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta");
+
+let arr = new Array(20);
+arr[0] = 'Sam' ;
+arr[1] = 'Jaime';
+arr[2] = 'Aime';
+arr[3] = 'Rogelio';
+arr[4] = 'Alejandro';
+arr[5] = 'Arturo';
+arr[6] = 'Julia';
+arr[7] = 'Roberto';
+arr[8] = 'Roberta';
+arr[9] = 'Sebastian';
+arr[10] = 'Francisco';
+arr[11] = 'Mario';
+arr[12] = 'Erick';
+arr[13] = 'Bernardo';
+arr[14] = 'Mauricio';
+arr[15] = 'Jack';
+arr[16] = 'Mohamed';
+arr[17] = 'Darwin';
+arr[18] = 'Luis';
+arr[19] = 'Andrés';
+
+for(s of arr){
+    console.log(s);
+}
