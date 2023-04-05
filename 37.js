5188c897abb93e12ff1a1841f83ad9c98a00b852
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 21:42:47 -0600

    Create 37.js
    Cargar las notas de los alumnos de un colegio en función del número de cursos y del número de alumnos por curso

---
+++ 37.js
@@ -0,0 +1,34 @@
+console.log("Cargar las notas de los alumnos de un colegio en función del número de cursos y del número de alumnos por curso");
+
+let materias = {
+    ingles: [
+        { nombre: 'Paco', calificacion: 3 },
+        { nombre: 'Maria', calificacion: 7 },
+        { nombre: 'Josefina', calificacion: 6 }
+    ],
+    español: [
+        { nombre: 'Paco', calificacion: 8 },
+        { nombre: 'Maria', calificacion: 8 },
+        { nombre: 'Jose', calificacion: 10 },
+        { nombre: 'Mariana', calificacion: 7 }
+    ]
+    ,
+    frances:
+        [{ nombre: 'Natalia', calificacion: 9 }]
+}
+
+let ls = [];
+for (materia in materias) {
+    ls.push(materias[materia]);
+}
+
+ls.sort((a,b) => {
+    if(a.length === b.length) return 0;
+    return (a.length < b.length) ? -1 : 1;
+});
+
+ls.forEach((materia) => {
+    materia.forEach((alumno) => {
+        console.log("Nombre: "+ alumno.nombre ", calificiacion: "+ alumno.calificacion);
+    });
+});
