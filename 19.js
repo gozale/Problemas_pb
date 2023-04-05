cf0b49c39a0816a25efe279f1cfdd7c750816f49
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 19:30:13 -0600

    Create 19.js
    Hacer un programa que simule el funcionamiento de un reloj digital y que permita ponerlo en hora

---
+++ 19.js
@@ -0,0 +1,9 @@
+console.log("Hacer un programa que simule el funcionamiento de un reloj digital y que permita ponerlo en hora");
+
+function mostrarTiempo(){
+  let f = new Date();
+  let m = f.toTimeString().substr(0,8);
+  console.log(m);
+  setTimeout(displayTime,10000);
+}
+displayTime();
