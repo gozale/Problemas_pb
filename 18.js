316629bdbaf3b45c0c9020736aa046bcb1d71ac6
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 19:18:17 -0600

    Create 18.js
    Hacer un programa que cuente las veces que aparece una determinada letra en una frase que introduciremos por teclado

---
+++ 18.js
@@ -0,0 +1,28 @@
+console.log("Hacer un programa que cuente las veces que aparece una determinada letra en una frase que introduciremos por teclado");
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let frasecita="";
+let letr='';
+let cont=0;
+
+rl.question("Ingresa la frase",(frase_inp) =>{
+  frasecita=frase_inp;
+
+rl.question("Ingresa la letra a buscar en la frase",(letra) =>{
+  letr=letra;
+
+  for(let l of fracesita){
+    if(l===letr) cont++;
+  }
+  console.log("La letra "+letr+" aparece "+cont+" veces en la frase "+ fracesita);
+  rl.close();
+});
+});
+
+rl.prompr();
