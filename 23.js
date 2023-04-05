7002dd7dc752e8657447b7068c706453cbdc1988
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 19:58:13 -0600

    Update 23.js

--- 23.js
+++ 23.js
@@ -1,5 +1,5 @@
 console.log("Hacer un programa que imprima los números del 0 al 100, controlando las filas y las columnas");
 
 for(let i=1; i<=10; i++){
-  console.log(`${i + 10} ${i + 20} ${i + 30} ${i + 40} ${i + 50} ${i + 60} ${i + 70} ${i + 80} ${i + 90}`);
+  console.log(`${i} ${i + 10} ${i + 20} ${i + 30} ${i + 40} ${i + 50} ${i + 60} ${i + 70} ${i + 80} ${i + 90}`);
 }
