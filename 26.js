c85d7ee3b99434ff9471e84322922aa42320ecf2
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 20:26:19 -0600

    Create 26.js
    Introducir una frase por teclado e imprimirla en el centro de la pantalla

---
+++ 26.js
@@ -0,0 +1,30 @@
+console.log("Introducir una frase por teclado e imprimirla en el centro de la pantalla");
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+function show(frase){
+  const screen = rl.screen({
+    smartCSR: true
+  });
+  
+  const text = rl.text({
+    content: 'ASAS',
+    top: 'center',
+    left: 'center',
+    align: 'center',
+    valign: 'middle'
+  });
+  screen.append(text);
+  screen.render();
+
+}
+
+readline.question('Introduce una frase: ', (frase) => {
+  mostrar(frase);
+  readline.close();
+});
