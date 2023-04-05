e4a39c5a2f0877af6a883e79f20fa9ae2dcd6809
gozale <124909575+gozale@users.noreply.github.com>
2023-04-04 20:40:17 -0600

    Create 29.js
    Simular cien tiradas de dos dados y contar las veces que entre los dos suman 10

---
+++ 29.js
@@ -0,0 +1,10 @@
+console.log("Simular cien tiradas de dos dados y contar las veces que entre los dos suman 10");
+
+let cont = 0;
+for(let i = 0; i < 100;i++){
+    let d1 = 1 + Math.round(Math.random()*5);
+    let d2 = 1 + Math.round(Math.random()*5);
+    if (d1 + d2 == 10) cont++;
+
+}
+console.log("Número de veces que suman 10: "+cnt);
